00b15c9decfa581f49acc7a36bfa24ac620e1d95
Tamim <tamimhossain031@gmail.com>
2024-11-02 15:44:49 +0600

    add scroll smooth

--- src/components/Helpers/ScrollToTop.js
+++ src/components/Helpers/ScrollToTop.js
@@ -5,6 +5,6 @@ export default function ScrollToTop(){
     const {pathname} = useLocation();
 
     useEffect(()=>{
-        window.scrollTo(0,0);
+        window.scrollTo({top: 0, left: 0, behavior: "smooth" });
     },[pathname])
 }
